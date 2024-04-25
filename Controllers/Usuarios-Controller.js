de21e9c40e6a3ba3b7d1dbd0c467564aab09e950
DevOps-PcNay <pcnay2003@outlook.com>
2024-04-25 14:51:13 -0700

    Se realiza la seccion que corresponde a la encriptacion de la contrasena Usuario

--- Controllers/Usuarios-Controller.js
+++ Controllers/Usuarios-Controller.js
@@ -1,8 +1,50 @@
 const { response, request } = require('express')
 const Usuario = require ('../Models/Usuario');
+const bcryptjs = require('bcryptjs')
 
 
 // Cuando se agrega esta variable, nos mostrara el menu de la funcion que se desestructuro de "expres" llamada "Response"
+
+const usuariosPost = async (req,res = response) =>
+{
+	// Obtiene lo que se envia en el "body" de la peticion POST.
+	// Se limpia para evitar inyeccion de SQL, entre otras validaciones.
+	//const envio_Body = req.body; 
+	// const {nombre, edad } = req.body
+	// const body_contenido  = req.body;
+	const {nombre,correo,password,rol } = req.body;
+	
+	// Si en el contenido del "Body" se manda una coleccion(campo) que no existe "Mongoose" lo ignora y se grabara en el documento (tabla)
+	const usuario_instancia = new Usuario({nombre,correo,password,rol});
+
+	// Antes de encriptar la contrasena se debe realizar lo siguiente:
+	// Verificar si el correo existe:
+
+
+	// Encriptar la contrasena (Hash de la contrasena)
+	// Este 10 es el numero de veces que geneerar la contrasena, entre mas mgrande se ale numero es mas dificil de que la decifren pero es mas tardado.
+	// Por defecto es 10 si no se especifica un valor
+	const salt = bcryptjs.genSaltSync(10) 
+	// .hashSync() = Encriptacion de una sola via.
+	// password = Es el que se destructuro en "const {nombre,correo,password,rol } = req.body;"
+	usuario_instancia.password = bcryptjs.hashSync(password,salt)
+
+
+
+	// Para grabar la Collecion en el Documento 
+	// Esperar que termine de grabar
+	// Si no se manda en el Body del POST las colecciones(campos) completo se queda con el mensaje de grabando, sin que termine, hasta que se aborte el programa.
+	await usuario_instancia.save();
+
+
+	res.status(201).json({
+		ok:true,
+		msg:'POST API - Controllers',
+		//nombre,
+		//edad
+		usuario_instancia
+	});
+}
 
 const usuariosGet = (req = request, res = response) => 
 {
@@ -41,31 +83,6 @@ const usuariosDel = (req,res = response) =>
 	res.json({
 		ok:true,
 		msg:'delete API - Controllers'
-	});
-}
-const usuariosPost = async (req,res = response) =>
-{
-	// Obtiene lo que se envia en el "body" de la peticion POST.
-	// Se limpia para evitar inyeccion de SQL, entre otras validaciones.
-	//const envio_Body = req.body; 
-	// const {nombre, edad } = req.body
-	const body_contenido  = req.body;
-	
-	// Si en el contenido del "Body" se manda una coleccion(campo) que no existe "Mongoose" lo ignora y se grabara en el documento (tabla)
-	const usuario_instancia = new Usuario(body_contenido);
-
-	// Para grabar la Collecion en el Documento 
-	// Esperar que termine de grabar
-	// Si no se manda en el Body del POST las colecciones(campos) completo se queda con el mensaje de grabando, sin que termine, hasta que se aborte el programa.
-	await usuario_instancia.save();
-
-
-	res.status(201).json({
-		ok:true,
-		msg:'POST API - Controllers',
-		//nombre,
-		//edad
-		usuario_instancia
 	});
 }
 
