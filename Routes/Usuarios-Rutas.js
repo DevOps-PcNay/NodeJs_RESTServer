dd257d39e70bc2d2a46f1d8c918534117d8f2dea
DevOps-PcNay <pcnay2003@outlook.com>
2024-05-17 17:30:06 -0700

    Actualizando el mantemiento de Usuarios

--- Routes/Usuarios-Rutas.js
+++ Routes/Usuarios-Rutas.js
@@ -3,7 +3,7 @@ const router = Router();
 
 // Para realizar validaciones varias, entre ellas del Email
 const { check } = require('express-validator')
-const Role = require('../Models/Role')
+const { Role } = require('../Models/Role')
 
 
 
@@ -26,21 +26,25 @@ router.post('/',[
 	check('nombre','El nombre es obligatorio').not().isEmpty(), // no debe estar vacio
 	check('password','El password debe ser de mas de 6 caracteres').isLength({ min:6}),
 	check('correo','El correo no es valido').isEmail(),
-	check('rol','Rol no valido').isIn(['ADMIN_ROLE','USER_ROLE']),
-	// Esta funcion revisa todos los errores generados por todos los middleware(check) anteriores, si los encuentra hasta que llegaria y no pasa al controllers "suariosPost"
-	validarCampos 	//const { validarCampos } = require('../Middlewares/Validar_campos');
-	
-	/*
+	// check('rol','Rol no valido').isIn(['ADMIN_ROLE','USER_ROLE']), Se desactiva porque la validacion sera a la Base de Datos.	
+	// Esta validando con la base de datos de MongoDB	
 	check('rol').custom(async (rol = '') => {
-		const existeRol = await Role.findOne({rol})	// Busca ek "Rol" en la Coleccion de "Roles"
+		const existeRol = await Role.findOne({rol});	// Busca ek "Rol" en la Coleccion de "Roles"		
+
 		if (!existeRol) // Si no existe
 		{
 			// Para manejar los errores personalizados.
 			// Que es atrapado en el "custom"
-			throw new Error(`El Rol ${ rol} no esta registrado en la Base De Datos`) 
+			throw new Error(`El Rol ${ rol } no esta registrado en la Base De Datos`) 
 		}
+		else
+		{
+			// alert("Se encontro")
+			console.log("Se encontro !!!! ")
+		}	
 	}),
-	*/
+		// Esta funcion revisa todos los errores generados por todos los middleware(check) anteriores, si los encuentra hasta que llegaria y no pasa al controllers "suariosPost"
+		validarCampos 	//const { validarCampos } = require('../Middlewares/Validar_campos');	
 
 ],usuariosPost)		// NO se coloca "()", es solo una referencia.
 
